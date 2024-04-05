9e69a04b3dc2b6dc190e3f7c454d068758f37640
Raffaele Sarracino <work@raffaelesarracino.it>
2024-04-05 15:31:41 +0200

    Init demo project

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NgxProgressLoadComponent } from '../../../ngx-progress-load/src/public-api';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NgxProgressLoadComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'demo';
+
+  progressValue: number = 25;
 }
