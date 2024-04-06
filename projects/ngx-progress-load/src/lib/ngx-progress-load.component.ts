2e7eaaf376ed68c0c9792072e44eaf4993fc7471
Raffaele Sarracino <work@raffaelesarracino.it>
2024-04-06 19:37:32 +0200

    Improved progress loader component

--- projects/ngx-progress-load/src/lib/ngx-progress-load.component.ts
+++ projects/ngx-progress-load/src/lib/ngx-progress-load.component.ts
@@ -1,28 +1,58 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ProgressLoaderDLService } from './partials/services/progress-loader-dl.service';
+import { Component, Input, OnInit, Type, ViewChild } from '@angular/core';
+import { AsyncPipe, NgComponentOutlet } from '@angular/common';
 
-import { CircleProgressComponent } from './partials/circle-progress/circle-progress.component';
+import { CircleProgressComponent } from './partials/components/circle-progress/circle-progress.component';
+import { SquareProgressComponent } from './partials/components/square-progress/square-progress.component';
 
-import { ProgressType } from './partials/enums';
+import { ColorType, ProgressType } from './partials/constants';
+import { ProgressLoader } from './partials/progress-loader';
+
+import { ProgressType as PType, ProgressColor as PColor } from './types/ngx-progress-load';
 
 @Component({
   selector: 'ngx-progress-load',
   standalone: true,
   imports: [
-    CircleProgressComponent
+    NgComponentOutlet,
+    AsyncPipe,
+    CircleProgressComponent,
+    SquareProgressComponent
   ],
   templateUrl: './ngx-progress-load.component.html',
   styleUrl: './ngx-progress-load.component.scss',
 })
-export class NgxProgressLoadComponent implements OnInit {
+export class NgxProgressLoadComponent implements OnInit, ProgressLoader {
+
+  @ViewChild('loader') private loader!: ProgressLoader; 
+
   @Input() value!: number
-  @Input() type: ProgressType = ProgressType.Circle;
-  @Input() color: 'orange' | 'blue' | 'green' | 'purple' = 'blue';
+  @Input() type: PType = ProgressType.circle;
+  @Input() color: PColor = ColorType.blue;
   @Input() showStatus: boolean = false;
   @Input() infinite: boolean = false;
 
+  protected loadComponent!: Type<ProgressLoader>;
+
+  constructor(
+    private progressLoaderDynamic: ProgressLoaderDLService
+  ) {}
 
   ngOnInit(): void {
-      
+       this.loadComponent = this.progressLoaderDynamic.getComponent(this.type);
+  }
+
+  protected get loaderInputs(): any {
+    return {
+      value: this.value,
+      color: this.color,
+      showStatus: this.showStatus,
+      infinite: this.infinite
+    };
+  }
+
+  runAnimation(): void {
+    this.loader?.runAnimation(); 
   }
 
 }
