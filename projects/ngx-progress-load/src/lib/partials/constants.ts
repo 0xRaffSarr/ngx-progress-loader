f7a80e1da248d47045a20f0edee0ceeb62871ee5
Raffaele Sarracino <work@raffaelesarracino.it>
2024-04-06 19:36:41 +0200

    added new constants file

---
+++ projects/ngx-progress-load/src/lib/partials/constants.ts
@@ -0,0 +1,11 @@
+export const ProgressType = {
+    circle : 'circle',
+    square: 'square'
+} as const;
+  
+export const ColorType = {
+    blue: 'blue',
+    orange: 'orange',
+    green: 'green',
+    purple: 'purple'
+} as const;
